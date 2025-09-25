ff51b8ccc0f39a20be90672d308a201ebda487af
Korolev Dmitry <dmkorolev@microsoft.com>
2025-09-25 19:41:06 +0200

    define type

---
+++ dotnet/src/Microsoft.Extensions.AI.Agents.Hosting.OpenAI.Responses/Model/StreamingResponse.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Extensions.AI.Agents.Hosting.Responses.Model;
+
+public class StreamingResponse
+{
+    [JsonPropertyName("type")]
+    public required StreamingResponseType Type { get; set; }
+
+    [JsonPropertyName("response")]
+    public required Response Response { get; set; }
+}
+
+public enum StreamingResponseType
+{
+    [JsonStringEnumMemberName("response.created")]
+    Created,
+
+    [JsonStringEnumMemberName("response.in_progress")]
+    InProgress,
+
+    [JsonStringEnumMemberName("response.completed")]
+    Completed,
+
+    [JsonStringEnumMemberName("response.failed")]
+    Failed,
+
+    [JsonStringEnumMemberName("response.incomplete")]
+    Incomplete
+}
